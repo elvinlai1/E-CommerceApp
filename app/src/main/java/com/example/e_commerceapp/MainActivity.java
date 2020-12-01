96186449bf4c9104bf1897a8bd863866a40b910e
uhjey <61769259+uhjey@users.noreply.github.com>
2020-12-01 14:48:23 -0800

    Coming Soon Added

--- app/src/main/java/com/example/e_commerceapp/MainActivity.java
+++ app/src/main/java/com/example/e_commerceapp/MainActivity.java
@@ -55,9 +55,6 @@ public class MainActivity extends AppCompatActivity {
                         startActivity(new Intent(MainActivity.this, ShoppingCart.class));
                         break;
                     case R.id.menu_shoes:
-                        Toast.makeText(MainActivity.this,"Coming Soon.",Toast.LENGTH_SHORT).show();
-                        drawerLay.closeDrawer(GravityCompat.START);
-                        break;
                     case R.id.menu_hats:
                         Toast.makeText(MainActivity.this,"Coming Soon.",Toast.LENGTH_SHORT).show();
                         drawerLay.closeDrawer(GravityCompat.START);
