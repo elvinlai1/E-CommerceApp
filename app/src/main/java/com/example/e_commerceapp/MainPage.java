a0ccc8e6c873618d3177885dd2282bf8b52f2033
uhjey <61769259+uhjey@users.noreply.github.com>
2020-11-30 21:56:07 -0800

    test push for nav drawer

--- app/src/main/java/com/example/e_commerceapp/MainPage.java
+++ app/src/main/java/com/example/e_commerceapp/MainPage.java
@@ -1,16 +1,48 @@
 package com.example.e_commerceapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainPage extends AppCompatActivity {
+    NavigationView nav;
+    DrawerLayout drawerLay;
+    Toolbar toolbar;
+    ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_page);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        nav = findViewById(R.id.navmenu);
+        drawerLay = findViewById(R.id.drawer);
+
+        toggle = new ActionBarDrawerToggle(this,drawerLay,toolbar,R.string.open,R.string.close);
+        drawerLay.addDrawerListener(toggle);
+        toggle.syncState();
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch(menuItem.getItemId()){
+                    case R.id.menu_cart:
+                        startActivity(new Intent(MainPage.this,Checkout.class));
+
+                }
+                return true;
+            }
+        });
     }
 }
-
-
