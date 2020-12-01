9ba781d87c140f5488d81513d4ef6bcbb94c3e88
elvinlai1 <61441620+elvinlai1@users.noreply.github.com>
2020-12-01 00:10:20 -0800

    Firebase added
    Signed-off-by: elvinlai1 <61441620+elvinlai1@users.noreply.github.com>

--- app/src/main/java/com/example/e_commerceapp/Checkout.java
+++ app/src/main/java/com/example/e_commerceapp/Checkout.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 /** Back Button
  * Grab Item details:
  *             - Costs
@@ -38,5 +41,50 @@ public class Checkout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
     }
+
+    
+
+
+    /**
+     *
+     * private void onStarClicked(DatabaseReference postRef) {
+     *     postRef.runTransaction(new Transaction.Handler() {
+     *         @Override
+     *         public Transaction.Result doTransaction(MutableData mutableData) {
+     *             Post p = mutableData.getValue(Post.class);
+     *             if (p == null) {
+     *                 return Transaction.success(mutableData);
+     *             }
+     *
+     *             if (p.stars.containsKey(getUid())) {
+     *                 // Unstar the post and remove self from stars
+     *                 p.starCount = p.starCount - 1;
+     *                 p.stars.remove(getUid());
+     *             } else {
+     *                 // Star the post and add self to stars
+     *                 p.starCount = p.starCount + 1;
+     *                 p.stars.put(getUid(), true);
+     *             }
+     *
+     *             // Set value and report transaction success
+     *             mutableData.setValue(p);
+     *             return Transaction.success(mutableData);
+     *         }
+     *
+     *         @Override
+     *         public void onComplete(DatabaseError databaseError, boolean committed,
+     *                                DataSnapshot currentData) {
+     *             // Transaction completed
+     *             Log.d(TAG, "postTransaction:onComplete:" + databaseError);
+     *         }
+     *     });
+     * }
+     */
 }
