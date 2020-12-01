77c05376641794b36dc2ab26f6cc4aa1404faf33
mikevong11 <vong.mike11@gmail.com>
2020-12-01 12:43:17 -0800

    Merge branch 'Alpha' of https://github.com/elvinlai1/E-CommerceApp
     Conflicts:
    	app/src/main/java/com/example/e_commerceapp/MainActivity.java

--- app/src/main/java/com/example/e_commerceapp/Checkout.java
+++ app/src/main/java/com/example/e_commerceapp/Checkout.java
@@ -5,8 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
 
 /** Back Button
  * Grab Item details:
